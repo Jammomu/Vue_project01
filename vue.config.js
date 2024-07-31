6fbcba646fb8b38ff5c43551762c053e622e49c1
Jammomu <pridom03@naver.com>
2024-07-31 10:15:22 +0900

    배포연습

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+// 빌드 할 때 dist를 docs로 바꿔줌
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/VUE_PROJECT/" : "/",
+  outputDir: "docs",
+};
