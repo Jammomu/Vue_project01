1f0fd6e4695c91f02f39ee7f4d54dec5803a9d3c
Jammomu <pridom03@naver.com>
2024-08-01 16:24:01 +0900

    이메일,비번 입력

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,52 +1,43 @@
 <template>
   <div class="">
-    <table>
-      <thead>
-        <th>제품명</th>
-        <th>가격</th>
-        <th>품목</th>
-        <th>배송료</th>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in goods" v-bind:key="index">
-          <td>{{item.name}}</td>
-          <td>{{item.price}}</td>
-          <td>{{item.category}}</td>
-          <td>{{item.delivery}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <select name="" id="">
+      <option value="서울">서울</option>
+      <option value="부산">부산</option>
+      <option value="대구">대구</option>
+      <option value="수원">수원</option>
+    </select>
     <br>
-
-    <p v-if="true">호랑이</p>
-    <p v-if="false">사자</p>
+    
+    <select v-model="city" @change="changeCity">
+      <option value="서울">서울</option>
+      <option value="부산">부산</option>
+      <option value="대구">대구</option>
+      <option value="수원">수원</option>
+    </select>
+    <br>
+    <p>{{message}}</p>
     <br>
 
     <div>
-      <label>국어 : <input type="number" v-model="kor"></label><br>
-      <label>영어 : <input type="number" v-model="eng"></label><br>
-      <label>수학 : <input type="number" v-model="math"></label><br>
-      <button v-on:click="showScore">계산</button>
-      <p>평균 : {{totalScore}}</p>
-      <p v-if="totalScore >= 90">A학점</p>
-      <p v-else-if="totalScore >= 80">B학점</p>
-      <p v-else-if="totalScore >= 70">C학점</p>
-      <p v-else-if="totalScore >= 60">D학점</p>
-      <p v-else>E학점</p>
+      <p>
+        <label>이메일
+          <input type="text" placeholder="이메일을 입력하세요" v-model="emailValue" @input="changeEmail">
+        </label>
+        <span>{{msgEmail}}</span>
+      </p>
+      <p>
+        <label>비밀번호
+          <input type="text" placeholder="이메일을 입력하세요" v-model="pwd1" @input="changePwd">
+        </label>
+        <span>{{msgPwd1}}</span>
+      </p>
+      <p>
+        <label>비밀번호 확인
+          <input type="text" placeholder="이메일을 입력하세요" v-model="pwd2" @input="changePwd2">
+        </label>
+        <span>{{msgPwd2}}</span>
+      </p>
     </div>
-
-    <div>
-      <button v-on:click="showMsg">메세지버튼</button>
-    </div>
-    <br>
-
-    <div>
-      <label><input type="number" v-model="ndan">단</label>
-      <button v-on:click="calculate_btn">계산</button>
-      <p>{{ndan}}단</p>
-      <p v-for="(item, index) in dan" :key="index">{{item}}</p>
-    </div>
-
   </div>
 </template>
 
@@ -58,46 +49,98 @@ export default {
   },
   data() {
     return {
-      goods: [
-        {'name': '마우스', price: 30000, 'category': 'PC용품', 'delivery': 2500},
-        {'name': '키보드', price: 100000, 'category': 'PC용품', 'delivery': 2500},
-        {'name': '모니터', price: 500000, 'category': 'PC용품', 'delivery': 2500},
-      ],
+      city: '대구',
+      message: '',
 
-      score: 0,
-      kor: 0,
-      eng: 0,
-      math: 0,
-      totalScore: 0,
-      ndan: '',
-      dan: [],
+      msgEmail:'이메일을 입력하세요',
+      pwd1: '',
+      pwd2: '',
+      msgPwd1: '비밀번호를 입력하세요.',
+      msgPwd2: '비밀번호를 확인하세요.',
     };
   },
   methods: {
-    showMsg(){
-      alert('메세지 전송');
-    },
-
-    showScore(){
-      this.totalScore = (this.kor + this.eng + this.math)/3;
-    },
-
-    calculate_btn(){
-      this.dan=[]; // 결과값 초기화
-      for(let n = 2; n < 10; n++){
-        this.dan.push(`${this.ndan} * ${n} = ${this.ndan * n}`);
+    changeCity(){
+      switch(this.city){
+        case '서울':
+          this.message= '안녕하세요. 반갑습니다.';
+          break;
+        case '부산':
+          this.message= '안녕하십니꺼. 반갑습니더.';
+          break;
+        case '대구':
+          this.message= '안녕하십니까. 반갑습니다.';
+          break;
+        case '수원':
+          this.message= '안녕하세요. 반갑습니다.';
+          break;
+        default:
+          this.message= '안녕하세요. 반갑습니다.';
       }
+    },
+
+    changeEmail(){
+      // this.emailValue이 이메일 형식에 맞는지 체크
+      const emailExp =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //문자열 + @ + 문자열 + . + 문자열
+      if(this.emailValue.length == 0){
+        this.msgEmail='이메일을 입력하세요';
+      } else if (emailExp.test(this.emailValue) == true ) {
+        this.msgEmail = '정상적인 이메일 주소입니다.';
+      } else {
+        this.msgEmail = '이메일 주소가 올바르지 않습니다.';
+      }
+    },
+
+    checkPwd(pwd){
+      let msg = '';
+      const pwdLength = this.pwd1.length;
+      const hasWord = /[a-z A-Z]/.test(pwd);
+      const hasNum = /[\d]/.test(pwd);
+      const hasForbid = /[@#]/.test(pwd);
+
+      if(pwdLength < 8){
+        msg = '비밀번호는 8자리 이상이어야 합니다.';
+      }
+      else if(!hasWord){
+        msg = '비밀번호에는 문자가 포함되어야 합니다.';
+      }
+      else if(!hasNum){
+        msg = '비밀번호에는 숫자가 포함되어야 합니다.';
+      }
+      else if(hasForbid){
+        msg = '@, #은 사용할 수 없습니다.';
+      }
+      else{
+        msg = '';
+      }
+
+      return msg;
+    },
+
+    changePwd(){
+      let msg = this.checkPwd(this.pwd1);
+      if(msg === ''){
+        msg = '비밀번호가 유효합니다.';
+      }
+      this.msgPwd1 = msg;
+    },
+
+    changePwd2(){
+      let msg = this.checkPwd(this.pwd2);
+      if(msg == ''){
+        if(this.pwd1 === this.pwd2){
+          msg = '비밀번호가 일치합니다.';
+        }
+        else{
+          msg = '비밀번호가 일치하지 않습니다.';
+        }
+      }
+      this.msgPwd2 = msg;
     },
   }
 };
 </script>
 
 <style scoped>
-table{
-  width: 100%;
-  border-collapse: collapse;
-}
-th, td{
-  border: 1px solid #ddd;
-}
+
 </style>
